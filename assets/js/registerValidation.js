85d5e546333144ee2df2016d0543aa6f5044c8e4
crazyyy <crazyyy@gmail.com>
2017-02-16 15:00:14 +0200

    clean

---
+++ assets/js/registerValidation.js
@@ -0,0 +1,40 @@
+﻿function submitRegisterForm() {
+    $('#socialNetworkError').hide();
+    var isValid = true;
+
+    if (isUniqueEmail()) {
+        $("#uniqueEmailError").hide();
+    }
+    else {
+        isValid = false;
+        $('#actionForm').valid();
+        $("#uniqueEmailError").show();
+    }
+
+    if (isValid) {
+        if ($('#actionForm').valid()) {
+            openPopupManual('popup-loading', 0.8, true);
+            $('#actionForm').submit();
+
+        }
+    }
+}
+
+function isUniqueEmail() {
+    return isUniqueEmailRequest($('#Email').val());
+}
+
+function isUniqueEmailRequest(email) {
+    var isValid = false;
+    $.ajax({
+        type: "POST",
+        url: '/Account/IsUniqueEmail',
+        dataType: "json",
+        data: '&email=' + email,
+        async: false,
+        success: function (response) {
+            isValid = response;
+        }
+    });
+    return isValid;
+}
